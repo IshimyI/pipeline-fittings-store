173361a89345a754bc79af49fc04d572ec7e1ad6
IshimyI <ivan.borisenko.2003@mail.ru>
2025-01-15 15:22:07 +0300

    addings imgs, 2nd page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// # region burger
+
 const burger = document.querySelector(".header-menu");
+const menuBurger = document.querySelector(".burger-menu");
 
 burger.addEventListener("click", () => {
   document.querySelector(".span1").classList.toggle("rotate-45");
@@ -6,7 +9,9 @@ burger.addEventListener("click", () => {
   document.querySelector(".span2").classList.toggle("opacity-0");
   document.querySelector(".span3").classList.toggle("-rotate-45");
   document.querySelector(".span3").classList.toggle("-translate-y-3");
-
-  //   burger.classList.toggle("");
-  //   -rotate-45rotate-45
+  menuBurger.classList.toggle("translate-x-96");
 });
+
+// # endregion
+
+// # region
