3102f94952e8bc64b90f6f88299cb2fc5d254a16
IshimyI <ivan.borisenko.2003@mail.ru>
2025-03-11 01:11:30 +0300

    fix

---
+++ server/db/database.js
@@ -0,0 +1,27 @@
+require("dotenv").config(); // Загружаем переменные окружения
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "123",
+    database: process.env.DB_NAME || "krioarmatura",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || "5433",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "123",
+    database: process.env.DB_NAME || "krioarmatura",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || "5433",
+    dialect: "postgres",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL", // Используем переменную окружения для Render
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false }, // Нужно для подключения через SSL (для Render)
+    },
+  },
+};
