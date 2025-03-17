613a5835e8ad5ecb2616b4e2530cc10151d3ee29
IshimyI <ivan.borisenko.2003@mail.ru>
2025-03-17 23:17:37 +0300

    rel

--- server/db/database.js
+++ server/db/database.js
@@ -16,18 +16,31 @@ module.exports = {
     },
   },
   test: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASS || "123",
-    database: process.env.DB_NAME || "krioarmatura",
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || "5432",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
     dialectOptions: {
-      ssl: { rejectUnauthorized: false },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 };
