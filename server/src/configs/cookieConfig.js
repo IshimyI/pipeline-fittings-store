12d7af04209d478ad8583a7e3e95e24da8bbd3f0
IshimyI <ivan.borisenko.2003@mail.ru>
2025-04-05 00:50:23 +0300

    refresh fix

--- server/src/configs/cookieConfig.js
+++ server/src/configs/cookieConfig.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const jwtConfig = require("./jwtConfig");
 
 const cookieConfig = {
   httpOnly: true,
   maxAge: jwtConfig.refresh.expiresIn,
   sameSite: process.env.COOKIE_SAME_SITE || "none",
-  secure: process.env.NODE_ENV === 'production',
+  secure: process.env.NODE_ENV === "production",
   path: process.env.COOKIE_PATH || "/",
-  domain: process.env.DOMAIN || 'localhost',
 };
 
 module.exports = cookieConfig;
